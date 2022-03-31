a25385df3aa417bb66dae747da5c4bc926ebda7f
Alex <v.superalex@gmail.com>
2022-03-31 17:15:34 +0300

    2

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,3 +1,5 @@
 function modal() {
     
 }
+
+module.exports = modal;
--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,7 @@
-window.addEventListener('DOMContentLoaded', function() {
-    const tabs = require('./modules/tabs'),
-          modal = require('./modules/modal'),
-          timer = require('./modules/timer'),
-          cards = require('./modules/cards'),
-          calc = require('./modules/calc'),
-          forms = require('./modules/forms'),
-          slider = require('./modules/slider');
 
-    tabs();
-    modal();
-    timer();
-    cards();
-    calc();
-    forms();
-    slider();
+window.addEventListener('DOMContentLoaded', function() {
+    const dropdown = require('./modules/dropdown');
+          
+    dropdown();
+   
 });
