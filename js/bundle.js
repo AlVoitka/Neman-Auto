eaf41cd581bc91493526ecc1bbb2ac8975d7646a
Alex <v.superalex@gmail.com>
2022-05-10 16:20:38 +0300

    +dots_slider

--- js/bundle.js
+++ js/bundle.js
@@ -576,14 +576,43 @@ function sliderJS() {
 
     let slideIndex = 1;
     
+    const indicators = document.querySelector('.aboutAs__indicators');
     const slides = document.querySelectorAll('.aboutAs__sliderJS-item');
     const prev = document.querySelector('.aboutAs__sliderJS-prev');
     const next = document.querySelector('.aboutAs__sliderJS-next');
     const total = document.querySelector('#total');
     const current = document.querySelector('#current');
+    const dots = [];
 
+    
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            width: 70px;
+            height: 15px;
+            margin-right: 10px;
+            margin-left: 10px;
+            cursor: pointer;
+            background-color: aqua;
+            border-radius: 50%;
+            background-clip: padding-box;
+            border-color: white;
+            border: 2px;
+            opacity: .3;
+            transition: opacity .6s ease;
+        `;
 
-
+        if (i == 0) {
+            dot.style.backgroundColor = 'aqua';
+            dot.style.opacity = 1;
+        }
+        
+        indicators.append(dot);
+        dots.push(dot);
+    }
+       
 
     showSlides(slideIndex);
 
@@ -593,7 +622,6 @@ function sliderJS() {
     } else {
         total.textContent = slides.length;
     }
-
 
 
     function showSlides(n) {
@@ -615,8 +643,11 @@ function sliderJS() {
         }
     }
 
+
     function plusSlides (n) {
         showSlides(slideIndex += n);
+        dots.forEach(dot => dot.style.opacity = ".5");
+        dots[slideIndex-1].style.opacity = 1;
     }
 
 
@@ -627,6 +658,29 @@ function sliderJS() {
     next.addEventListener('click', function(){
         plusSlides(1);
     });
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            showSlides();
+
+            slideIndex = slideTo;
+
+            showSlides();
+
+            if (slides.length < 10) {
+                current.textContent =  `0${slideIndex}`;
+            } else {
+                current.textContent =  slideIndex;
+            }
+
+            
+
+            dots.forEach(dot => dot.style.opacity = ".5");
+            dots[slideIndex-1].style.opacity = 1;
+        })
+    })
 
 
 
